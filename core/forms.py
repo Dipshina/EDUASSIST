f17418f88f387e118d201328ffa033371ef28dd8
Dipshina <dipshinatamang@gmail.com>
2024-09-18 13:21:14 +0545

    Updates

--- core/forms.py
+++ core/forms.py
@@ -13,6 +13,28 @@ class UserRegisterForm(UserCreationForm):
         model = User
         fields = ['username', 'first_name', 'last_name', 'email', 'password1', 'password2']
 
+    def __init__(self, *args, **kwargs):
+        super(UserRegisterForm, self).__init__(*args, **kwargs)
+        # Remove help texts for specific fields
+        self.fields['username'].help_text = ''
+        self.fields['password1'].help_text = ''
+        self.fields['password2'].help_text = ''
+        
+        # Customize error messages for password fields
+        self.fields['password1'].widget.attrs.update({
+            'placeholder': 'Enter your password',
+        })
+        self.fields['password2'].widget.attrs.update({
+            'placeholder': 'Confirm your password',
+        })
+        self.fields['password1'].error_messages = {
+            'required': 'Please provide a password.',
+        }
+        self.fields['password2'].error_messages = {
+            'required': 'Please confirm your password.',
+            'password_mismatch': 'The two password fields didn’t match.',
+        }
+
     def clean_email(self):
         email = self.cleaned_data.get('email')
         if User.objects.filter(email=email).exists():
